ec312189d5bbc684415e45236b56c955f3046cb9
0ni1ecram <ndewaiyo@gmail.com>
2023-07-31 19:45:09 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" contnet="width=device-width,initial-scale=1.0">
+<!--// Bootstrap Links -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<!-- // Font Awesome CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+
+
+<!--// cdn JQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js">
+
+</body>
+</html>
