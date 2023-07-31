65f0450ca870ab2b3a60ea27d74950ac173f64f8
0ni1ecram <ndewaiyo@gmail.com>
2023-07-31 20:06:12 +0300

    The last change got blocked

---
+++ partials/modal.php
@@ -0,0 +1,6 @@
+<div class="modal" aria-label="modal" id="usermodal">
+    <form>
+        <label for="file">Photo</label>
+        <input type="file" id="file" aria-labelledby="" placeholder="Choose a picture">
+    </form>
+</div>
